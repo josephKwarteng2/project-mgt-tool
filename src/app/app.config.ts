017f0119c63a5f892d4552ef75784fa7b79c92e9
josephKwarteng2 <josephkwarteng244@gmail.com>
2024-12-10 11:40:22 +0000

    chore: register store in app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideStore } from '@ngxs/store';
+import { LoginState } from './state/login/login.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideStore([]),
+    provideStore([LoginState]),
   ],
 };
